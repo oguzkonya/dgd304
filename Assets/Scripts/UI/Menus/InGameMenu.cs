b509ce400fa1e546e81a9a3854fe6c0c39e70c6d
Omer Faruk Istıibal <ofarukistikbal@hotmail.com>
2022-05-17 10:29:16 +0300

    Refactor Menu, implement Observer pattern and delete MenuManager

--- Assets/Scripts/UI/Menus/InGameMenu.cs
+++ Assets/Scripts/UI/Menus/InGameMenu.cs
@@ -5,17 +5,29 @@ using UnityEngine.UI;
 
 public class InGameMenu : Menu
 {
+
     public Button pauseButton;
 
     protected override void OnInitialize()
     {
         pauseButton.onClick.AddListener(OnPauseButtonClicked);
+        Main.Instance.MainHandle += OnMainUpdated;
+    }
+
+    private void OnMainUpdated(MainContext context)
+    {
+        switch (context)
+        {
+            case MainContext.Play:
+                Show();
+                break;
+        }
     }
 
     private void OnPauseButtonClicked()
     {
-        Time.timeScale = 0;
-        menuManager.Show<PauseMenu>();
+        //Time.timeScale = 0;
+        //menuManager.Show<PauseMenu>();
     }
 
     // TODO: Show air
